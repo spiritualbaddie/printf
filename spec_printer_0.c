ff75ba170616c036cfdca814da5cff29630a32ad
Charles George <charleskevoregeorge@gmail.com>
2023-04-25 06:23:26 -0700

    task

--- spec_printer_0.c
+++ spec_printer_0.c
@@ -18,11 +18,11 @@ void convert_fmt_c(va_list *args_list, fmt_info_t *fmt_info)
 		for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
 			_putchar(' ');
 	}
-		_putchar(str);
-		if (fmt_info->left)
+	_putchar(str);
+	if (fmt_info->left)
 	{
 		for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-		_putchar(' ');
+			_putchar(' ');
 	}
 }
 
@@ -40,18 +40,18 @@ void convert_fmt_s(va_list *args_list, fmt_info_t *fmt_info)
 
 	str = str ? str : null_str;
 	len = fmt_info->is_precision_set && fmt_info->prec >= 0
-	? fmt_info->prec : str_len(str);
+		? fmt_info->prec : str_len(str);
 	if (!fmt_info->left)
 	{
 		for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
 			_putchar(' ');
 	}
 	for (i = 0; i < len && *(str + i) != '\0'; i++)
-	_putchar(*(str + i));
+		_putchar(*(str + i));
 	if (fmt_info->left)
 	{
 		for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-		_putchar(' ');
+			_putchar(' ');
 	}
 }
 
@@ -61,8 +61,8 @@ void convert_fmt_s(va_list *args_list, fmt_info_t *fmt_info)
  * @args_list: The arguments list
  * @fmt_info: The format info
  */
-void convert_fmt_percent(va_list *arg_list, fmt_info_t *fmt_info)
+void convert_fmt_percent(va_list *args_list, fmt_info_t *fmt_info)
 {
-	(void)arg_list;
+	(void)args_list;
 	_putchar(fmt_info->spec);
 }
