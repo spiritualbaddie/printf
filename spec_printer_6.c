7fe139ae34d95f30fc1b0a8c0419095dfca9fd05
Charles George <charleskevoregeorge@gmail.com>
2023-04-25 05:38:20 -0700

    specifier

--- spec_printer_6.c
+++ spec_printer_6.c
@@ -10,32 +10,32 @@
  */
 void convert_fmt_p(va_list *args_list, fmt_info_t *fmt_info)
 {
-        int i, len;
-        void *ptr = va_arg(*args_list, void *);
-        char *str = ptr_to_str(ptr);
+	int i, len;
+	void *ptr = va_arg(*args_list, void *);
+	char *str = ptr_to_str(ptr);
 
-        (void)fmt_info;
-        if (str)
-        {
-                len = str_len(str);
-                if (!fmt_info->left)
-                {
-                        for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-                                _putchar(' ');
-                }
-                for (i = 0; *(str + i) != '\0'; i++)
-                        _putchar(*(str + i));
-                if (fmt_info->left)
-                {
-                        for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-                                _putchar(' ');
-                }
-                free(str);
-        }
-        else
-        {
-                _putstr("(nil)");
-                if (str)
-                        free(str);
-        }
+	(void)fmt_info;
+	if (str)
+	{
+		len = str_len(str);
+		if (!fmt_info->left)
+		{
+			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+				_putchar(' ');
+		}
+		for (i = 0; *(str + i) != '\0'; i++)
+			_putchar(*(str + i));
+		if (fmt_info->left)
+		{
+			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+				_putchar(' ');
+		}
+		free(str);
+	}
+	else
+	{
+		_putstr("(nil)");
+		if (str)
+			free(str);
+	}
 }
